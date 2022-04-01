4cfdcbae004fb1a91bf6997afb0dd9972784e8ee
Cplan082 <46877705+Cplan082@users.noreply.github.com>
2022-04-01 12:28:50 -0400

    quick test of custom.py

--- deliverables/del_e/childClass_test/project_test.py
+++ deliverables/del_e/childClass_test/project_test.py
@@ -4,12 +4,16 @@
 # sys.path.append(os.path.realpath('../../../base_project'))
 
 # from BrachioGraphError import BrachioGraphError
-
+import time
 from custom import bg
 
 # bg = BrachioGraphError(
 #     servo_1_parked_pw=1475,
 #     servo_2_parked_pw=1350)
+
+
+bg.set_angles(-90,80)
+time.sleep(3)
 
 bg.park()
 
