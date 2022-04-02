24a4a5422479ccedbceb6d4a4f7205445293df11
Nick-Bailuc <nick.bailuc@gmail.com>
2022-04-02 14:32:34 -0400

    new data

---
+++ deliverables/del_e/childClass_test/box.py
@@ -0,0 +1,43 @@
+# import os
+# import sys
+
+# sys.path.append(os.path.realpath('../../../base_project'))
+
+# from BrachioGraphError import BrachioGraphError
+import time
+import RPi.GPIO as GPIO
+import data_collection_class as dc
+from custom import bg
+arduino_int0_trig_pin = 4
+bg.park()
+
+#GPIO.setwarnings(False)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(arduino_int0_trig_pin, GPIO.OUT)
+#GPIO.output(arduino_int0_trig_pin, False)
+
+csv_name = 'hyster_dataset_clean'
+i = 1
+time.sleep(1)
+bg.box(bounds=[-6,11,6,20])
+time.sleep(1)
+bg.park()
+#while True:
+#    input("start test")
+#    data_coms = dc.data_collection('/dev/ttyACM0',
+#                                   './datasets/'+csv_name+f'_{i}.csv',
+#                                   ['Shoulder', 'Elbow'])
+#    time.sleep(2) # Gives time for data_coms to initialise
+    
+#    bg.park()
+#    time.sleep(1)
+    
+#    GPIO.output(arduino_int0_trig_pin, True)
+#    bg.test_pattern(both=True) # hysteresis correcton test call
+#    GPIO.output(arduino_int0_trig_pin, False)
+#    data_coms.stop_listening()
+    
+#    bg.park()
+#    i+=1
+    
+    # bg.plot_file("circle.json")
