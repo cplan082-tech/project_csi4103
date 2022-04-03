fa6c8166f38721517e74d05a1eaec775789e3de4
Olivier Maillet Glas <oliviermailletglas@gmail.com>
2022-04-03 17:13:21 -0400

    results of angles_2

--- deliverables/del_e/pot_calibration/main_plot_potentiometers.py
+++ deliverables/del_e/pot_calibration/main_plot_potentiometers.py
@@ -2,5 +2,5 @@ import plot
 import xycoordinates
 
 """MAIN CODE TO CONVERT ANGLES OF POTENTIOMETERS TO IMAGE"""
-xycoordinates.main("angles_1.csv") # Launch xycoordinates.py
+xycoordinates.main("angles_2.csv") # Launch xycoordinates.py
 plot.main() #Launch plot.py
