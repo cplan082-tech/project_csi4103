43c79c606f307fe165a1073d4e6bd95f0319ed4a
Nick Bailuc <nick.bailuc@gmail.com>
2022-04-03 15:46:37 -0400

    modified return.py again

--- deliverables/del_e/pot_calibration/return.py
+++ deliverables/del_e/pot_calibration/return.py
@@ -3,11 +3,17 @@
 
 # sys.path.append(os.path.realpath('../../../base_project'))
 
-# from BrachioGraphError import BrachioGraphError
 import time
 import RPi.GPIO as GPIO
 import data_collection_class as dc
 from custom import bg
+arduino_int0_trig_pin = 40
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(arduino_int0_trig_pin, GPIO.OUT)
+GPIO.output(arduino_int0_trig_pin, False)
 
 
 arduino_int0_trig_pin = 40
