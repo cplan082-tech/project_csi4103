02e276946991327938363c3273e9aff9c826cd3a
Nick-Bailuc <nick.bailuc@gmail.com>
2022-04-03 22:32:00 -0400

    differences

--- deliverables/del_e/childClass_test/BrachioGraphError.py
+++ deliverables/del_e/childClass_test/BrachioGraphError.py
@@ -117,7 +117,7 @@ class BrachioGraphError(BrachioGraph):
 
         # determine if error is acceptable (for now, test with 1 degree error maximum)
         while (shoulder_error > error_eps or elbow_error > error_eps):
-            time.sleep(1)
+            time.sleep(1.5)
 
             if (shoulder_error > error_eps):
                 shoulder_angle = shoulder_angle + proportional_controller(shoulder_angle_anchor,shoulder_pot)
